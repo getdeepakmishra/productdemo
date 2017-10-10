fcf074b5ee04b35f51872d20c3109490002ab20c
dpux <dux.deepak@gmail.com>
2017-10-09 19:23:08 -0500

    readded app module for easy setup

--- app/src/main/java/me/deepak/demoapp/LauncherActivity.java
+++ app/src/main/java/me/deepak/demoapp/LauncherActivity.java
@@ -1,13 +1,17 @@
 package me.deepak.demoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import me.deepak.search.SearchActivity;
 
 public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launcher);
+        startActivity(new Intent(this, SearchActivity.class));
+        finish();
     }
 }
